6a5b0fec2de2e5afaaae835f534cc622ee24aa48
Mark Winterbottom <mark@londonappdeveloper.com>
2019-03-04 20:12:30 +0000

    Reversed filtering tags and ingredients by assigned

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -18,12 +18,7 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
 
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
-        assigned_only = bool(self.request.query_params.get('assigned_only'))
-        queryset = self.queryset
-        if assigned_only:
-            queryset = queryset.filter(recipe__isnull=False)
-
-        return queryset.filter(user=self.request.user).order_by('-name')
+        return self.queryset.filter(user=self.request.user).order_by('-name')
 
     def perform_create(self, serializer):
         """Create a new object"""
