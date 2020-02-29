f42a15c4ef450f6dddc13a67368fe6a3e20f4926
Mark Winterbottom <mark@winterbottom.me>
2020-02-29 07:26:49 -0500

    Fixed issue with password too short test passing if password is long enough.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -46,7 +46,11 @@ class PublicUserApiTests(TestCase):
 
     def test_password_too_short(self):
         """Test that the password must be more than 5 characters"""
-        payload = {'email': 'test@londonappdev.com', 'password': 'pw'}
+        payload = {
+            'email': 'test@londonappdev.com',
+            'password': 'pw',
+            'name': 'Test',
+        }
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
