1929b5f3715fe3c457835adc0f7e31a48227c4f5
Mark Winterbottom <mark@londonappdeveloper.com>
2021-04-30 11:49:16 +0100

    Upgrade PostgreSQL to version 13.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -69,7 +69,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
             ingredient_ids = self._params_to_ints(ingredients)
             queryset = queryset.filter(ingredients__id__in=ingredient_ids)
 
-        return queryset.filter(user=self.request.user)
+        return queryset.filter(user=self.request.user).order_by('-id')
 
     def get_serializer_class(self):
         """Return appropriate serializer class"""
